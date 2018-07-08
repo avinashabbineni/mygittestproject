bf8edce7d811cbf4c93a789e6c44da27e28e1a8e
avinash <avinashabbineni@gmail.com>
2018-07-08 14:43:16 -0400

    initial changes for sum

---
+++ GitTestProject/src/com/testproject/model/Sum.java
@@ -0,0 +1,12 @@
+package com.testproject.model;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+	}
+
+}
